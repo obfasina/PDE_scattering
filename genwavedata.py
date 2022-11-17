189fb129a6ed6c6b43ba79867fca5ca2b0621ba7
obfasina <dami@aditik>
2022-11-17 12:42:54 -0800

    Cleaning script (script is used to generate synthetic data with wave eqn)

---
+++ genwavedata.py
@@ -0,0 +1,473 @@
+from __future__ import print_function
+from mshr import *
+from fenics import *
+import numpy as np
+import fenics
+import sys
+#from mshr import *
+import matplotlib.pyplot as plt
+import dill
+import pickle
+import argparse
+import pandas as pd
+
+
+gendata = True
+analyzedata=False
+
+if gendata == True:
+
+    #Generation of wave velocity parameters
+
+
+    #Number of different wave velocity parameters
+    trainsamp = 10 
+    alpharange = np.random.normal(0,1,trainsamp) #1,1
+    betarange = np.random.normal(0,1,trainsamp) #1,1
+    gammarange = np.random.normal(0,1,trainsamp) #80,20
+
+    coord = np.load("gauss_ellipse_coord.npy") #NOTE need to have coordinates a pirori for simulation to work
+    coord = np.load("gauss_polygon_coord.npy")
+
+    #Gaussians
+    gauss = 1
+    if gauss == 0:
+        incondp = []
+        for i in range(trainsamp):
+
+            pcenterx = np.random.normal(0,np.max(coord[:,0]),1)
+            pcentery = np.random.normal(0,np.max(coord[:,1]),1)
+            ncoordx = np.abs(coord[:,0] - pcenterx)
+            ncoordy = np.abs(coord[:,1] - pcentery)
+            ncoord = np.concatenate((ncoordx.reshape(coord.shape[0],1),ncoordy.reshape(coord.shape[0],1)),axis=1)
+
+            val = gammarange[i]*np.exp(-alpharange[i]*(ncoord[:,0] + ncoord[:,1]))
+            incondp.append(np.abs(val))
+
+    #Polynomials
+    else:
+        incondp = []
+        for i in range(trainsamp):
+            #initmesh = np.random.normal(loc=100,scale=1.0,size=nnodes) # initial condition with 1000 random values
+
+            pcenterx = np.random.normal(0,np.max(coord[:,0]),1)
+            pcentery = np.random.normal(0,np.max(coord[:,1]),1)
+            ncoordx = np.abs(coord[:,0] - pcenterx)
+            ncoordy = np.abs(coord[:,1] - pcentery)
+            ncoord = np.concatenate((ncoordx.reshape(coord.shape[0],1),ncoordy.reshape(coord.shape[0],1)),axis=1)
+
+
+            val = (1 + (alpharange[i]*ncoord[:,0]**2) + (betarange[i]*ncoord[:,1]**2))
+            incondp.append(np.abs(val))
+
+
+
+
+
+    for i in range(trainsamp): #Simulate wave equation for different wave velocity parameters
+
+        #Simulation parameters
+        num_steps = 10
+        dt = 1E-1
+
+        #Determine mesh
+        # Create mesh and define function space
+        #nx = ny = 50
+        #mesh = RectangleMesh(Point(0,0),Point(1,1),nx, ny)
+
+        domain_vertices = [Point(0.0,0.7),Point(0.0,-0.35),Point(1.0,-1.0),Point(2.5,0.0),Point(1.0,1.0)]#,Point(0.7,-1.0)]
+        domain = Polygon(domain_vertices)
+        mesh = generate_mesh(domain,10)
+        File('polygon.xml') << mesh
+        mesh = Mesh('polygon.xml')
+        
+        domain = Ellipse(Point(0,0),2,3)
+        mesh = generate_mesh(domain,16)
+        File('ellipse.xml') << mesh
+        mesh = Mesh('ellipse.xml')
+
+
+        V = FunctionSpace(mesh, 'P', 1)
+        coord = V.tabulate_dof_coordinates()
+
+        def boundary(x, on_boundary):
+            return on_boundary
+        bc = DirichletBC(V, Constant(0), boundary)
+
+        # set initial condition
+        u_n = interpolate(Constant(10), V)
+        u_n_n = interpolate(Constant(0),V)
+        
+        #set wave velocity parameter
+        c = interpolate(Constant(0),V)
+        c.vector()[:] = incondp[i].squeeze()[:]
+        #c = 5
+
+        # Define variational problem
+        u = TrialFunction(V)
+        v = TestFunction(V)
+        F = u*v*dx + dt*dt*c*c * dot(grad(u),grad(v))*dx - (2*u_n - u_n_n)*v*dx
+        a, L = lhs(F), rhs(F)
+
+
+        # Time-stepping
+        u = Function(V)
+        t = 0
+        field_val = []
+        num_steps = 10
+        for n in range(num_steps):
+
+            
+            # Update current time
+            #t += dt
+            # Compute solution
+            solve(a == L, u, bc)
+
+            # Update previous solution
+            u_n_n.assign(u_n)
+            u_n.assign(u)
+
+            #print(u_n.vector().get_local())
+            field_val.append(u_n.vector().get_local())
+            
+        field_val = np.array(field_val)
+
+
+        #gauss_ellipse_sol[i,:,:] = field_val
+        fig, axs = plt.subplots(2,5, figsize=(20,5))
+        axs = axs.ravel()
+        fig.subplots_adjust(hspace = 0.5, wspace=1)
+
+        for i in range(num_steps):
+            
+            axs[i].set_title("(T =" + str(i) + ")")
+            img = axs[i].scatter(coord[:,0],coord[:,1],c = field_val[i], vmin = np.min(field_val[-1]), vmax = np.max(field_val[-1]))
+            axs[i].set_xlabel(" X ")
+            axs[i].set_ylabel(" Y ")
+            plt.colorbar(img)
+
+
+    fig, axs = plt.subplots(2,5, figsize=(20,5))
+    axs = axs.ravel()
+    fig.subplots_adjust(hspace = 0.5, wspace=1)
+
+    for i in range(num_steps):
+        
+        axs[i].set_title("(Param: (" + str(i) + ")")
+        img = axs[i].scatter(coord[:,0],coord[:,1],c = field_val[i])
+        axs[i].set_xlabel(" X ")
+        axs[i].set_ylabel(" Y ")
+        plt.colorbar(img)
+
+
+        
+
+    
+
+
+    sys.exit()
+
+    #print(gauss_ellipse_sol.shape)
+    #print(gauss_ellipse_sol[8,40,:])
+    #np.save("poly_polygon_sol.npy",gauss_ellipse_sol)
+
+
+    plt.figure()
+    plt.title(" Gaussian IC 1")
+    plt.scatter(coord[:,0],coord[:,1],c = incondpload[1])
+    plt.colorbar()
+
+    plt.figure()
+    plt.title("Gaussian IC 4")
+    plt.scatter(coord[:,0],coord[:,1],c = incondpload[4])
+    plt.colorbar()
+
+    plt.figure()
+    plt.title("Gaussian IC 7")
+    plt.scatter(coord[:,0],coord[:,1],c = incondpload[7])
+    plt.colorbar()
+
+    plt.figure()
+    plt.title("Gaussian Field value (T=25,IC=1)")
+    plt.scatter(coord[:,0],coord[:,1],c = gauss_ellipse_sol[1,24,:].squeeze())
+    plt.colorbar()
+
+    plt.figure()
+    plt.title("IC 4")
+    plt.title("Gaussian Field value (T=25,IC=4)")
+    plt.scatter(coord[:,0],coord[:,1],c = gauss_ellipse_sol[4,24,:].squeeze())
+    plt.colorbar()
+
+    plt.figure()
+    plt.title("Gaussian Field value (T=25,IC=7)")
+    plt.scatter(coord[:,0],coord[:,1],c = gauss_ellipse_sol[7,24,:].squeeze())
+    plt.colorbar()
+
+    # Normalize color scale
+
+    plt.figure()
+    plt.title(" Gaussian IC 1 (Same scale) ")
+    plt.scatter(coord[:,0],coord[:,1],c = incondpload[1],vmin = np.min(incondpload[1]), vmax = np.max(incondpload[1]) )
+    plt.colorbar()
+
+    plt.figure()
+    plt.title("Gaussian IC 4 (Same scale) ")
+    plt.scatter(coord[:,0],coord[:,1],c = incondpload[4],vmin=np.min(incondpload[1]) , vmax = np.max(incondpload[1]))
+    plt.colorbar()
+
+    plt.figure()
+    plt.title("Gaussian IC 7 (Same scale) ")
+    plt.scatter(coord[:,0],coord[:,1],c = incondpload[7], vmin = np.min(incondpload[1]) , vmax = np.max(incondpload[1]))
+    plt.colorbar()
+
+    plt.figure()
+    plt.title("Gaussian Field value (T=25,IC=1) (Same scale) ")
+    plt.scatter(coord[:,0],coord[:,1],c = gauss_ellipse_sol[1,24,:].squeeze(), vmin = np.min(gauss_ellipse_sol[1,24,:].squeeze()), vmax = np.max(gauss_ellipse_sol[1,24,:].squeeze()) )
+    plt.colorbar()
+
+    plt.figure()
+    plt.title("IC 4")
+    plt.title("Gaussian Field value (T=25,IC=4) (Same scale) ")
+    plt.scatter(coord[:,0],coord[:,1],c = gauss_ellipse_sol[4,24,:].squeeze(), vmin = np.min(gauss_ellipse_sol[1,24,:].squeeze()),vmax = np.max(gauss_ellipse_sol[1,24,:].squeeze()) )
+    plt.colorbar()
+
+    plt.figure()
+    plt.title("Gaussian Field value (T=25,IC=7) (Same scale) ")
+    plt.scatter(coord[:,0],coord[:,1],c = gauss_ellipse_sol[7,24,:].squeeze(), vmin = np.min(gauss_ellipse_sol[1,24,:].squeeze()), vmax = np.max(gauss_ellipse_sol[1,24,:].squeeze()) )
+    plt.colorbar()
+
+
+
+    """
+    #CFL Check
+    vecdiff = np.abs(sol_time[-1] - sol_time[-2])
+    vecmean = np.abs(np.mean(sol_time[-1]))
+    relative = np.linalg.norm(vecdiff,2)/vecmean
+    print("Percent Error",relative*100)
+    print(sol_time[-1])
+
+    """
+
+
+
+
+
+#print("Difference")
+#print(incondp[0])
+#print(np.max(incondp[1]))
+#print(np.linalg.norm(np.abs(incondp[0] - incondp[2]),2)/np.mean(incondp[2]))
+#print(alpharange)
+
+
+#print(sol_time[-1])
+#np.save("poly_polygon_IC.npy",np.array(incondp))
+#np.save("poly_polygon_coord.npy",coord)
+#np.save("Gauss_ellipse_sol.npy",np.array(sol_time[-1]))
+
+
+#Define coordinate mesh grid (for visualization)
+#x = np.linspace(np.min(coord[:,0]),np.max(coord[:,0]),25)
+#y = np.linspace(np.min(coord[:,1]),np.max(coord[:,1]),25)
+#xx, yy = np.meshgrid(x,y)
+
+
+
+if analyzedata == True:
+
+
+    #load data from wandb
+    NNoutputpred = pd.read_csv('/Users/oluwadamilolafasina/Downloads/wandb_export_2022-11-01T11_27_37.166-07_00.csv').to_numpy()
+    NNoutputgt = pd.read_csv('/Users/oluwadamilolafasina/Downloads/wandb_export_2022-11-01T11_25_37.109-07_00.csv').to_numpy()
+    waveparamgt = NNoutputgt[:,0]  
+    waveparampred = NNoutputpred[:,0]  
+    print(waveparamgt)
+    print(waveparampred)
+
+
+    #Predicted
+
+
+    #run simulation
+    dt = 1E-1
+    num_steps = 50
+    # Create mesh and define function space
+    #nx = ny = 50
+    #mesh = RectangleMesh(Point(0,0),Point(1,1),nx, ny)
+
+    if np.sum(np.isnan(waveparampred)) > 0:
+
+        domain_vertices = [Point(0.0,0.7),Point(0.0,-0.35),Point(1.0,-1.0),Point(2.5,0.0),Point(1.0,1.0)]#,Point(0.7,-1.0)]
+        domain = Polygon(domain_vertices)
+        mesh = generate_mesh(domain,10)
+        File('polygon.xml') << mesh
+        mesh = Mesh('polygon.xml')
+        wave = waveparampred[:197]
+
+        V = FunctionSpace(mesh, 'P', 1)
+        coord = V.tabulate_dof_coordinates()
+       
+
+
+    if np.sum(np.isnan(waveparampred)) == 0:
+
+        domain = Ellipse(Point(0,0),2,3)
+        mesh = generate_mesh(domain,16)
+        File('ellipse.xml') << mesh
+        mesh = Mesh('ellipse.xml')
+
+        V = FunctionSpace(mesh, 'P', 1)
+        coord = V.tabulate_dof_coordinates()
+
+      
+
+
+    V = FunctionSpace(mesh, 'P', 1)
+    coord = V.tabulate_dof_coordinates()
+
+    def boundary(x, on_boundary):
+        return on_boundary
+    bc = DirichletBC(V, Constant(0), boundary)
+
+    # Define initial value
+    u_n = interpolate(Constant(10), V)
+    u_n_n = interpolate(Constant(5),V)
+    #beta = 5
+
+
+    c = interpolate(Constant(0),V)
+    c.vector()[:] = wave.squeeze()[:]
+    #c = 5
+
+    # Define variational problem
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    F = u*v*dx + dt*dt*c*c * dot(grad(u),grad(v))*dx - (2*u_n - u_n_n)*v*dx
+    a, L = lhs(F), rhs(F)
+
+
+    # Time-stepping
+    u = Function(V)
+    t = 0
+    field_val = []
+    for n in range(num_steps):
+
+        
+        # Update current time
+        #t += dt
+        # Compute solution
+        solve(a == L, u, bc)
+
+        # Update previous solution
+        u_n_n.assign(u_n)
+        u_n.assign(u)
+
+        #print(u_n.vector().get_local())
+        field_val.append(u_n.vector().get_local())
+        
+    pred_field = np.array(field_val)
+
+
+
+    #Ground Truth
+
+    #run simulation
+    dt = 1E-1
+    num_steps = 50
+    # Create mesh and define function space
+    #nx = ny = 50
+    #mesh = RectangleMesh(Point(0,0),Point(1,1),nx, ny)
+
+    if np.sum(np.isnan(waveparampred)) > 0:
+
+        domain_vertices = [Point(0.0,0.7),Point(0.0,-0.35),Point(1.0,-1.0),Point(2.5,0.0),Point(1.0,1.0)]#,Point(0.7,-1.0)]
+        domain = Polygon(domain_vertices)
+        mesh = generate_mesh(domain,10)
+        File('polygon.xml') << mesh
+        mesh = Mesh('polygon.xml')
+        wave = waveparampred[:197]
+
+        V = FunctionSpace(mesh, 'P', 1)
+        coord = V.tabulate_dof_coordinates()
+       
+
+
+    if np.sum(np.isnan(waveparampred)) == 0:
+
+        domain = Ellipse(Point(0,0),2,3)
+        mesh = generate_mesh(domain,16)
+        File('ellipse.xml') << mesh
+        mesh = Mesh('ellipse.xml')
+
+        V = FunctionSpace(mesh, 'P', 1)
+        coord = V.tabulate_dof_coordinates()
+
+      
+
+
+    V = FunctionSpace(mesh, 'P', 1)
+    coord = V.tabulate_dof_coordinates()
+
+    def boundary(x, on_boundary):
+        return on_boundary
+    bc = DirichletBC(V, Constant(0), boundary)
+
+    # Define initial value
+    u_n = interpolate(Constant(10), V)
+    u_n_n = interpolate(Constant(5),V)
+    #beta = 5
+
+
+    c = interpolate(Constant(0),V)
+    c.vector()[:] = wave.squeeze()[:]
+    #c = 5
+
+    # Define variational problem
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    F = u*v*dx + dt*dt*c*c * dot(grad(u),grad(v))*dx - (2*u_n - u_n_n)*v*dx
+    a, L = lhs(F), rhs(F)
+
+
+    # Time-stepping
+    u = Function(V)
+    t = 0
+    field_val = []
+    for n in range(num_steps):
+
+        
+        # Update current time
+        #t += dt
+        # Compute solution
+        solve(a == L, u, bc)
+
+        # Update previous solution
+        u_n_n.assign(u_n)
+        u_n.assign(u)
+
+        #print(u_n.vector().get_local())
+        field_val.append(u_n.vector().get_local())
+        
+    gt_field = np.array(field_val)
+
+
+plt.figure()
+plt.title(" Predicted Wave Velocity")
+plt.scatter(coord[:,0],coord[:,1],c = waveparamgt[:197])#,vmin = np.min(incondpload[1]), vmax = np.max(incondpload[1]) )
+plt.colorbar()
+
+plt.figure()
+plt.title(" Ground Truth Wave Velocity ")
+plt.scatter(coord[:,0],coord[:,1],c = waveparampred[:197])#,vmin = np.min(incondpload[1]), vmax = np.max(incondpload[1]) )
+plt.colorbar()
+
+
+plt.figure()
+plt.title(" Predicted Field Value (T=25)")
+plt.scatter(coord[:,0],coord[:,1],c = pred_field[2,:])#,vmin = np.min(incondpload[1]), vmax = np.max(incondpload[1]) )
+plt.colorbar()
+
+plt.figure()
+plt.title(" Ground Truth Field Value (T=25)")
+plt.scatter(coord[:,0],coord[:,1],c = gt_field[2,:])#,vmin = np.min(incondpload[1]), vmax = np.max(incondpload[1]) )
+plt.colorbar()
+
